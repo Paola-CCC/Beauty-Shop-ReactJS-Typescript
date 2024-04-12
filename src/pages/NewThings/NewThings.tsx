ed2cd52256fc79f280c89ff5198d6bc169d32eb4
Paola_C <paola.cyprien@hetic.net>
2024-04-12 19:23:15 +0200

    page nouveauté

--- src/pages/NewThings/NewThings.tsx
+++ src/pages/NewThings/NewThings.tsx
@@ -1,11 +1,51 @@
-import React  from 'react';
+import React, { useEffect, useState } from 'react';
 import './NewThings.scss';
+import { Products } from '../../types/products.type';
+import { useAppSelector } from '../../redux/hooks';
+import { selectAllProducts } from '../../redux/products/productsSlice';
+import { Card } from '../../components/ui'
+
+const NewThings = () => {
+  
+  const [productDatas,setProductDatas] = useState<Products[]>([]);
+  const products = useAppSelector(selectAllProducts);
 
 
-const NewThings = () => (
-  <div className="NewThings">
-    NewThings Component
-  </div>
-);
+  useEffect(() => {
+    
+      if( products ) {        
+        const datas = products.filter((e) => e.categories === "make-up");        
+        setProductDatas(datas);
+      }
+      
+  },[products])
+
+  
+ return (
+  <div className="container-lastest-product-zone">
+  
+  <section>
+    <div className="title-grp">
+      <h3> TOUTES NOS NOUVEAUTÉS </h3>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus cursus mattis porttitor. </p>
+    </div>
+  </section>
+
+  <section>
+    <ul className='items-list makeup'>
+        { productDatas?.map((e : Products,index: number) => (
+          <li key={index}>
+                <Card 
+                  path={'make-up/'+ e.id}
+                  {...e} 
+                />
+          </li>
+        ))} 
+    </ul>
+  </section>
+
+</div>
+)}
+;
 
 export default NewThings;
